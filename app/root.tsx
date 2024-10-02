9720cbd3cede52e7b8792a25fb5f0f9f7b739b19
sena-m09 <sena-murakami@gaji.jp>
2024-10-02 13:37:27 +0900

    fix root

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,14 @@
+import { type PropsWithChildren } from "react";
+import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
+  LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import { ConfigProvider, App as AntdApp, theme, Button, Empty } from "antd";
 import jaJP from "antd/lib/locale/ja_JP.js";
@@ -16,9 +19,10 @@ export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+
+export default function App() {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -26,15 +30,31 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <Empty />
-        {children}
+        <RootProvider>
+          <Empty />
+          <Outlet />
+        </RootProvider>
         <ScrollRestoration />
         <Scripts />
+        <LiveReload />
       </body>
     </html>
   );
 }
 
-export default function App() {
-  return <Outlet />;
-}
+const RootProvider = ({ children }: PropsWithChildren) => {
+  return (
+    <ConfigProvider
+      // TODO: locale の設定方法を見直す必要あり
+      // NOTE: [Internationalization | Ant Design 公式](https://ant.design/docs/react/i18n)に則って実装したものの、日本語化されず。いったん @ts-ignore を使用
+      // @ts-ignore
+      locale={jaJP}
+      theme={{
+        algorithm: theme.defaultAlgorithm,
+      }}
+      renderEmpty={() => <Empty />}
+    >
+      <AntdApp>{children}</AntdApp>
+    </ConfigProvider>
+  );
+};
